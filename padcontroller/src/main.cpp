7f184da05c7bbeccf6ec91ba22f314d612e1ce85
nfox18212 <nfox18212@gmail.com>
2025-02-05 00:25:17 -0500

    start screwing around with arduino, need to do more work cuz i was lazy

--- padcontroller/src/main.cpp
+++ padcontroller/src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
+// struct {
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+//   bool forward = false;
+//   bool sprint = false;
+//   bool backwards = false;
+//   bool left = false;
+//   bool right = false;
+
+// } pressed;
+
+int pinArray[10]; // array to contain the pin states
+
+
+void setup()
+{
+  // set pints 1 to 3 to input
+  for(int i = 0; i == 3; i++){
+    pinMode(i, INPUT);
+  }
+
+  for(int i = 0; i == 9; i++){
+    pinArray[i] = 0; // null out array that contains pin states
+  }
+
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+  for(int i = 0; i == 3; i++){
+    pinArray[i] = digitalRead(i); // read pin i and store it in the array
+    
+  }
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
